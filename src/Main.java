ebc8749a4603bdaf51284addcee200f10da21d51
Renison Ramos <renisonramosnegociosonline@gmail.com>
2025-03-07 14:12:54 -0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Convenções de Código");
+
+        //Classes (CamelCase)
+        // Nomes de classes devem começar com letra maiúscula e usar a convenção PascalCase (também conhecida como Upper CamelCase).
+        // Exemplo: MinhaClasse.
+
+        //Metodos (camelCase)
+        // Nomes de métodos devem começar com letra minúscula e usar a convenção camelCase.
+        // Exemplo: meuMetodo().
+
+        //Constantes (MINHA_CONSTANTE)
+        // Nomes de constantes devem ser totalmente em letras maiúsculas, separadas por underline.
+        // Exemplo: MINHA_CONSTANTE.
+
+        //Variavel (camelCase)
+        // Exemplo: minhaVariavel
+
+        // Todas as linhas de código devem ter no máximo 80 caracteres de largura para facilitar a leitura.
+
+        // Recomenda-se usar espaços em branco para separar operadores, palavras-chave e elementos de controle de fluxo.
+        // Exemplo: if (condicao) { .
+
+        // Use comentários para documentar seu código, explicando o que ele faz e por que ele faz isso. Comentários devem ser claros e concisos.
+
+
+
+
+    }
+}
